d57bb1cff4d297610ffe66eefaa4d04e359491f3
FarhanSadikAlways <farhansadik0760@gmail.com>
2024-12-10 07:48:48 -0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   index.html
    	modified:   javascript.js
    	modified:   style.css

--- javascript.js
+++ javascript.js
@@ -1,7 +1,60 @@
 var pBubble = document.querySelector("#pBubble");
+var num;
+var score = 0;
 
-for (var i = 0; i <= 215; i++) {
-  var num = Math.floor(Math.random() * 10);
-
-  pBubble.innerHTML += `<div class="bubble">${num}</div>`;
+function createBubble() {
+  let bubbles = "";
+  for (var i = 0; i <= 215; i++) {
+    num = Math.floor(Math.random() * 10);
+    bubbles += `<div class="bubble">${num}</div>`;
+  }
+  pBubble.innerHTML = bubbles;
 }
+
+function timerCode() {
+  var timer = 60;
+  var tt = document.querySelector("#timer");
+
+  function time() {
+    var interval = setInterval(function () {
+      if (timer >= 0) {
+        tt.textContent = timer;
+        timer--;
+      } else {
+        clearInterval(interval);
+        pBubble.innerHTML = "<h1>Game Is Over Bro😂</h1>";
+      }
+    }, 1000);
+  }
+
+  time();
+}
+
+function hitRandom() {
+  var hit = document.querySelector("#hit");
+  function hitt() {
+    hit.innerHTML = num;
+  }
+  hitt();
+}
+
+function scoref() {
+  score += 10;
+  console.log((document.querySelector("#score").textContent = score));
+}
+
+function bubbleClick() {
+  pBubble.addEventListener("click", function (dets) {
+    var bubbleNum = Number(dets.target.textContent);
+    if (bubbleNum === num) {
+      createBubble();
+      hitRandom();
+      scoref();
+    }
+  });
+}
+
+createBubble();
+hitRandom();
+timerCode();
+bubbleClick();
